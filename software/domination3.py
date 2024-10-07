571e11aaf1f30f65c5a7ca663b01406bff8b824e
Salvatore Stella <salvatore.stella@univaq.it>
2024-10-07 23:14:18 +0200

    some useless edits to the code

--- software/domination3.py
+++ software/domination3.py
@@ -22,8 +22,37 @@ def p_lambda_seq(B, la, seq):
                 new_Ps.append(Em*Pm)
         Ps = new_Ps
         B.mutate(k)
-    return Ps
+#    return Ps
     return PolyhedralComplex(Ps).union_as_polyhedron()
+
+def p_lambda_seq_as_list_any_rank(B, la, seq):
+    m = B.nrows()
+    rk = B.rank()
+    n = B.ncols()
+    B = block_matrix([[B,matrix(la).transpose()]])
+    for k in seq:
+        B.mutate(k)
+    B, la  = B[:,:-1], B[:,-1] 
+    Ps = [Polyhedron(rays=B.columns(),base_ring=QQ).translation(la)]
+    for k in reversed(seq):
+        Ep = E(B, k, 1)
+        Em = E(B, k, -1)
+        Hp = Polyhedron(ieqs=[(0,)*(k+1)+(1,)+(0,)*(m-k-1)])
+        Hm = Polyhedron(ieqs=[(0,)*(k+1)+(-1,)+(0,)*(m-k-1)])
+        new_Ps = []
+        for P in Ps:
+            Pp = P.intersection(Hp)
+            if Pp.dimension() == rk:
+                new_Ps.append(Ep*Pp)
+            Pm = P.intersection(Hm)
+            if Pm.dimension() == rk:
+                new_Ps.append(Em*Pm)
+        Ps = new_Ps
+        B.mutate(k)
+    P = Ps[0]
+    for Q in Ps:
+        P = P.convex_hull(Q)
+    return P
 
 def p_lambda_seq_convexhull(B, la, seq):
     m = B.nrows()
